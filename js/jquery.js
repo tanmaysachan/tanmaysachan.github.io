f49de979615de2d4bb7f4775b43f0c6d5d2b0fcf
Tanmay Sachan <tanmaysachan@Tanmays-MacBook-Air.local>
2017-07-21 18:14:17 +0530

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$("#imgdiv").stop().fadeIn(1000);
+	$("#maintext").stop().fadeIn(1000);
+	$("#intro-body").stop().delay(500).fadeIn(1000);
+	$("#icon-row").stop().delay(500).fadeIn(1000);
+});
