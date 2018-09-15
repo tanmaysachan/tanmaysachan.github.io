298ae33c2530402992625cac1f004f23837e6013
tanmaysachan <tnmysachan@gmail.com>
2018-09-15 11:48:07 +0530

    change fade-in time

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-	$("#imgdiv").stop().fadeIn(1000);
-	$("#maintext").stop().fadeIn(1000);
-	$("#intro-body").stop().delay(500).fadeIn(1000);
-	$("#icon-row").stop().delay(500).fadeIn(1000);
+	$("#imgdiv").stop().fadeIn(4000);
+	$("#maintext").stop().fadeIn(4000);
+	$("#intro-body").stop().delay(500).fadeIn(2000);
+	$("#icon-row").stop().delay(500).fadeIn(2000);
 });
