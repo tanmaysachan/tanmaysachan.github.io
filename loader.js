0bbaeaec9a53cd2f788b8656970739cd4fafe226
tanmaysachan <tnmysachan@gmail.com>
2023-04-09 11:48:11 +0530

    update site

---
+++ loader.js
@@ -0,0 +1,66 @@
+window.onload = function() {
+    if (window.location.pathname.split("/").pop() === "post.html") {
+        function retrieveStorage() {
+            var contentLink = null;
+            if (localStorage.getItem('content_link') === null) {
+                console.log('waiting');
+                setTimeout(retrieveStorage, 500);
+            } else {
+                contentLink = localStorage.getItem('content_link')
+                console.log(contentLink);
+            }
+            return contentLink;
+        }
+        var contentLink = retrieveStorage();
+
+        if (contentLink === null) {
+            console.log('failure to load');
+            return;
+        }
+
+        var converter = new showdown.Converter();
+
+        // fetch and inject html
+        fetch(contentLink)
+        .then(res => res.text())
+        .then(data => {
+            var htmlContent = converter.makeHtml(data);
+            var postBlock = document.getElementById("post-block");
+            postBlock.insertAdjacentHTML('beforeend', htmlContent);
+            furtherFormat();
+        })
+
+    } else {
+
+        var myList = document.getElementById("listOfPosts");
+
+        postsUrl = "https://api.github.com/repos/tanmaysachan/tanmaysachan.github.io/contents/posts"
+        rawContentUrl = "https://raw.githubusercontent.com/tanmaysachan/tanmaysachan.github.io/master/posts/"
+
+
+        fetch(postsUrl)
+        .then( res => res.json() )
+        .then( data => {
+            data.forEach(element => {
+                var newListItem = document.createElement("li");
+                var newListItemText = document.createTextNode(element.name);
+                var newListItemText = document.createElement("a");
+                newListItemText.innerText = element.name.slice(0, -3);
+                newListItemText.href = "post.html";
+                var contentLink = rawContentUrl + element.name;
+                newListItemText.onclick = function () {
+                    localStorage.setItem('content_link', contentLink);
+                    console.log(localStorage.getItem('content_link'));
+                }
+                newListItem.appendChild(newListItemText);
+                myList.appendChild(newListItem);
+            });
+        });
+    }
+}
+
+function furtherFormat() {
+    // Find the first h1 element and colour it blue
+    var firstH1 = document.getElementsByTagName("h1")[0];
+    firstH1.style = "color: #1b95e0;";
+}
